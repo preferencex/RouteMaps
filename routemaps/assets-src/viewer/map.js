78a1fe3dd59c208e5b7c9de28505dc84eb720b84
preferencex <45601124+preferencex@users.noreply.github.com>
2026-09-12 12:13:36 +0100

    fix: use MapLibre ESM namespace in viewer build

--- routemaps/assets-src/viewer/map.js
+++ routemaps/assets-src/viewer/map.js
@@ -1,4 +1,4 @@
-import maplibregl from 'maplibre-gl';
+import * as maplibregl from 'maplibre-gl';
 import { Protocol } from 'pmtiles';
 import { visiblePois } from './filters.js';
 import { __, sprintf } from '../shared/i18n.js';
